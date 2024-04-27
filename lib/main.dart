4d870cfd09e7ba15f4a6f0afa41e864ab894a65a
Drowsy-Kat <khizer78901@gmail.com>
2024-04-27 23:06:52 +0100

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:roberts_rodeo/theme/dark_theme.dart';
 import 'package:roberts_rodeo/theme/light_theme.dart';
 import 'app_body.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
